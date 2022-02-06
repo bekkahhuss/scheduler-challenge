0e12e47a5136a83d03df5396b1e1e941c50fdb03
bekkahhuss <bekkahhuss@gmail.com>
2022-02-06 14:35:50 -0500

    removed extra comments, spaces etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,23 @@
 var time = moment().format("dddd, MMMM Do YYYY, h:mm a");
-console.log(time);
-
 var timeLabels = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"];
-
-$("h4").each(function(index) {
-    $(this).text(timeLabels[index]);
-})
-// console.log(("h4").each);
 
 var displayTime = $("#currentDay");
 displayTime.text(time);
 
-
 var saveBtn = $(".saveBtn");
-
-
-
-// var hour = moment().format("H");
-// console.log(hour);
-
-
-
-
     
+$("h4").each(function(index) {
+    $(this).text(timeLabels[index]);
+})
+
+
 $("textarea").each(function() {
     var reloadTask = localStorage.getItem($(this).siblings("h4").attr("id"), $(this).siblings("textarea").val());
-    console.log(reloadTask);
     $(this).text(reloadTask);
 })
 
+
 $("textarea").each(function(index) {
-    //add 9 hours to the difference between index number and hour
-    // $(this).text("");
 
     if(index + 9 < moment().hours()) {
         $(this).addClass("past")
@@ -41,9 +27,7 @@ $("textarea").each(function(index) {
     }
     else {
         $(this).addClass("future")
-    }
-    
-   
+    } 
 })
 
 function saveTask (event) {
